45be81fe3b9082ded55ee1c848fc8c9a03d86716
Nahid2002S <viptechmaster@gmail.com>
2023-05-04 23:22:08 +0600

    all done

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,13 +3,8 @@ import { AuthContext } from '../authprovider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
-    const {user, loading} = useContext(AuthContext);
+    const {user} = useContext(AuthContext);
     const location = useLocation();
-    if(loading){
-        return <div>
-            <progress className="progress w-56"></progress>
-        </div>
-    }
     if(user){
         return children;
     }
