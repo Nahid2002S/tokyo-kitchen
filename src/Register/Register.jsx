3c89b7fd8481aff0b16a8187352019d719a07eb1
Nahid2002S <viptechmaster@gmail.com>
2023-05-04 23:33:50 +0600

    correction

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -26,7 +26,6 @@ const Register = () => {
 
         registerUser(email,password)
         .then(result => {
-            logOut()
             navigate('/login');
             updateUser(result.user, name, photoUrl)
         })
